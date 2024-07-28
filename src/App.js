77ab1331593458a2c9bd09226fcc095a1abadc9d
m0slah <muhammedslahqadir@gmail.com>
2024-07-28 11:05:50 +0300

    add provider

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
-  const [theme, colorMode] = useState();
+  const [theme, colorMode] = useState("");
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
